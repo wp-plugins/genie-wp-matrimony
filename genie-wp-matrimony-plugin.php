65362ef24d01787d280d5c098ca34994b843650c
prakashm88 <prakashm88@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-19 08:15:28 +0000

    dummy commit check
    git-svn-id: https://plugins.svn.wordpress.org/genie-wp-matrimony/trunk@897934 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- genie-wp-matrimony-plugin.php
+++ genie-wp-matrimony-plugin.php
@@ -3,10 +3,7 @@
 /*
 Plugin Name: Genie WP Matrimony
 Plugin URI: http://www.itechgenie.com/myblog/genie-wp-matrimony
-Description: Genie WP Matrimony is Wordpress plugin which help in converting your 
-Wordpress blog into a complete matrimonial website. This plugin uses the information 
-of the default Wordpress users which makes it very easier and faster integration 
-with existing Wordpress setup. 
+Description: Genie WP Matrimony is Wordpress plugin which help in converting your Wordpress blog into a complete matrimonial website. This plugin uses the information of the default Wordpress users which makes it very easier and faster integration with existing Wordpress setup. 
 Version: 0.7
 Author: prakashm88
 Author URI: http://www.itechgenie.com
